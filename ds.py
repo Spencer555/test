d118d3e367558d9a57929fdd9ec8218c19082078
Spencer555 <46141953+Spencer555@users.noreply.github.com>
2022-03-11 12:29:55 +0000

    Add files via upload

---
+++ ds.py
@@ -0,0 +1,68 @@
+
+#question 16
+# import requests
+# url = 'https://www.facebook.com/'
+# # params = {'address':"1235"}
+# results = requests.post(url=url, params ={"q":"search items"})
+# res = results
+
+# file = open("url.txt", 'a')
+# file.write(f"{url}/{params}")
+# file.close() 
+
+
+# to run python ds.py
+
+
+#question 21 # name = "John doe"
+# age = 15
+#
+# print (f"Name is {name} and age is {age}")
+
+
+#question 15
+# biscuit = int(37)
+# toffes = int(68)
+# total = int(0)
+# discount = int(0)
+
+# def get_product():
+#     select = input('Pls select product eg. 1 for biscuit only 2 for toffes 3 for all  :')
+    # amount = input("Pls enter the amount")
+    # quantity = input("Pls enter quantity  ")
+    # product_code = input("Pls enter product code")
+    # if int(select) == 1:
+    #     total = biscuit * int(quantity)
+
+    # if int(select) == 2:
+    #     total = toffes * int(quantity)
+    # if int(select) == 3:
+    #     total =  toffes + int(biscuit) * int(quantity)
+    # if int(select) > 3 and int(select) < 0:
+        # print('pls select the right input')
+
+    # discount = (int(total)/5)
+    # final_price = int(total) - int(discount)
+    # print ("discount", discount)
+    # print('final price', final_price)
+
+
+
+# get_product()
+
+
+
+
+
+
+# question 17 #import sys
+# file =  open("myself.txt", 'r')
+
+# data = file.readlines()
+# print(data[0])
+
+# print(len(data[0]))
+# print(sys.getsizeof(data[0]), 'bytes')
+
+
+
